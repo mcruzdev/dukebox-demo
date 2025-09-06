8385081aaef5ad5465bfe62b51177844f0f7349b
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-09-06 16:54:32 -0300

    Try to use outbox pattern from dapr

--- producer-app/src/main/java/io/dapr/quarkus/examples/producer/OrderResource.java
+++ producer-app/src/main/java/io/dapr/quarkus/examples/producer/OrderResource.java
@@ -1,6 +1,10 @@
 package io.dapr.quarkus.examples.producer;
 
 import io.dapr.client.DaprClient;
+import io.dapr.client.domain.ExecuteStateTransactionRequest;
+import io.dapr.client.domain.State;
+import io.dapr.client.domain.TransactionalStateOperation;
+import io.dapr.client.domain.TransactionalStateOperation.OperationType;
 import io.quarkus.logging.Log;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -8,9 +12,11 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Path("/orders")
 public class OrderResource {
-
 
     final DaprClient daprClient;
 
@@ -33,5 +39,28 @@ public class OrderResource {
         Log.info("Publishing Order Event: " + order);
         this.daprClient.publishEvent("pubsub", "topic", order).block();
         return "Order Stored and Event Published";
+    }
+
+    @POST
+    @Path("/outbox")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String outboxStoreOrder(Order order) {
+
+        ExecuteStateTransactionRequest executeStateTransactionRequest =
+                new ExecuteStateTransactionRequest("kvstore");
+
+        List<TransactionalStateOperation<?>> ops = new ArrayList<>();
+
+        TransactionalStateOperation<Order> upsertOps = new TransactionalStateOperation<>(OperationType.UPSERT,
+                new State<>(order.getId(), order, ""));
+
+        ops.add(upsertOps);
+
+        executeStateTransactionRequest.setOperations(ops);
+
+        this.daprClient.executeStateTransaction(executeStateTransactionRequest).block();
+
+        return "Order Stored and Event Published (via Outbox Pattern)";
     }
 }
